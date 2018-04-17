3d81a782c4f9afd0a8fa69cc2cee8647d324dc65
vinchinzu <lukevinze@gmail.com>
2018-04-17 16:31:50 -0400

    Update play_random_phrase.py

--- play_random_phrase.py
+++ play_random_phrase.py
@@ -6,28 +6,16 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 import random
 
-foo = ['Ciao bello, Buon Giorno', 'come stai ragazzo?', 'uno, due, tre, quattro, cinque, sei, sette, otto, nove, dieci', 'che vuoi fare oggi?', 'cinque, sei, sette, otto','sopra la panca la capra campa, sotto la panca la capra crepa',
+phrase_list = ['Ciao bello, Buon Giorno', 'come stai ragazzo?', 'uno, due, tre, quattro, cinque, sei, sette, otto, nove, dieci', 'che vuoi fare oggi?', 'cinque, sei, sette, otto','sopra la panca la capra campa, sotto la panca la capra crepa',
      'sai parlare Itaaliano?', 'hey Maambo, Maambo Italiano', 'Miinchia, braavo',
       'Sopra la panca la capra campa, sotto la panca la capra crepa', 'Michele aveva un gallo, bianco rosso e verde e giallo, e per farlo ben cantare, gli dava da mangiare']
-
 
 button = aiy.voicehat.get_button()
 led = aiy.voicehat.get_led()
 
-#path to wave file
-path2 = '/home/pi/audio/1523749823.wav'
-pp =    '/home/pi/audio/leo.wav'
-
 #play audio when button is pressed
-def say(words):
-	aiy.audio.say(words, lang='it-IT', volume=20)
-
-
-def audio():
-	print('Playing a test sound...')
-	aiy.audio.say('Buon Giorno, principe',lang='it-IT', volume=20,pitch=105)
-	#aiy.audio.play_wave(pp)
-	aiy.audio.say('Ciao Bella', lang='it-IT', volume=20)
+def say_italian(words):
+	aiy.audio.say(words, lang='it-IT', volume=20, pitch=100)
 
 while True:
 	button.wait_for_press()

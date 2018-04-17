c4e5f0752102424f94e2269427cf938b6d2f4733
vinchinzu <lukevinze@gmail.com>
2018-04-17 13:54:55 -0400

    Create HelloWorldAudio.py

---
+++ HelloWorldAudio.py
@@ -0,0 +1,8 @@
+import aiy.audio
+
+aiy.audio.say("Hello World")
+
+aiy.audio.say("Buon Giorno Principesa", lang = 'it-IT')
+
+#Great Britain english
+aiy.audio.say('some words to say', lang='en-GB')

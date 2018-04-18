17f96e4b0c614bfe8046cf6718d3a9063c151de6
vinchinzu <lukevinze@gmail.com>
2018-04-18 15:45:57 -0400

    Update setup.sh

--- mambo/setup.sh
+++ mambo/setup.sh
@@ -3,3 +3,5 @@
 youtube-dl --extract-audio --audio-format mp3 --output mambo.mp3 https://www.youtube.com/watch?v=UM1BYWEF3vY
 
 ffmpeg -i mambo.mp3 -acodec pcm_s16le -ac 1 -ar 16000 mambo.wav
+
+rm mambo.mp3
